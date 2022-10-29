46989eb5e4f8c26cf1ba2869967ab5c63f2eb0ed
Shivamm117 <shivam.mahajan117@gmail.com>
2022-10-29 14:44:59 +0530

    gg

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://Shivam:Shivam@book-store.unvv8kn.mongodb.net/?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,12 +26,22 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('6302842f1d219f59d660fa88')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
-      console.log(user);
       next();
     })
     .catch(err => console.log(err));
@@ -35,7 +53,7 @@ app.use(authRoutes)
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://Shivam:Shivam@book-store.unvv8kn.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI)
 .then(result => {
   User.findOne().then(user => {
     if(!user){
